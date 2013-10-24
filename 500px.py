4a41c86fc51bb188c10ffbe1282fc4124d1f1242
figen <figengungor1@gmail.com>
2013-10-24 13:12:07 +0300

    made the code user firendly

--- 500px.py
+++ 500px.py
@@ -1,15 +1,12 @@
-import urllib, urllib2
-link = raw_input("Enter the link: ")
-name = raw_input("And name: ")
+import urllib
+link = raw_input("Enter the photo link: ")
+name = raw_input("Enter a name for the photo: ")
 f = urllib.urlopen(link)
 pageResource = f.read()
 pattern="{\"size\":2048,\"url\":"
 start = pageResource.find(pattern)+20
 end = pageResource.find("\"", start+2)
-
 imgLink = pageResource[start:end]
-
 imgLink=imgLink.replace("\\", "")          
-print(imgLink)
-
 urllib.urlretrieve(imgLink, name+".jpg")
+print("Photo is successfully downloaded into the directory where your 500px.py file is.")
